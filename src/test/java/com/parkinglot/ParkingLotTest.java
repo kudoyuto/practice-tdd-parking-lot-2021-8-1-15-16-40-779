0d06c0278aa7598510388318b163d3acc06755ce
kudoyuto <yuto.kudo@oocl.com>
2021-08-06 14:53:40 +0800

    [ParkingLot][Yuto] test: Write test case for return_parked_car_when_fetch_given_parking_lot_with_park_car_and_parking_ticket

--- src/test/java/com/parkinglot/ParkingLotTest.java
+++ src/test/java/com/parkinglot/ParkingLotTest.java
@@ -2,7 +2,9 @@ package com.parkinglot;
 
 import org.junit.jupiter.api.Test;
 
+import static org.junit.jupiter.api.Assertions.assertEquals;
 import static org.junit.jupiter.api.Assertions.assertNotNull;
+
 
 public class ParkingLotTest {
     @Test
@@ -14,5 +16,16 @@ public class ParkingLotTest {
         ParkingTicket parkingTicket = parkingLot.park(car);
         ///Then
         assertNotNull(parkingTicket);
+    }
+    @Test
+    void return_parked_car_when_fetch_given_parking_lot_with_park_car_and_parking_ticket(){
+        //Given
+        Car car =new Car();
+        ParkingLot parkingLot = new ParkingLot();
+        ParkingTicket parkingTicket = parkingLot.park(car);
+        //When
+        Car actualCar = parkingLot.fetch(parkingTicket);
+        ///Then
+        assertEquals(car,actualCar);
     }
 }
