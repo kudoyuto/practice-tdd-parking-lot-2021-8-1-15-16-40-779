960d6817e7b1dd0fc5b7f55135b1f798796c4b34
kudoyuto <yuto.kudo@oocl.com>
2021-08-07 19:55:38 +0800

    [ParkingLot][Yuto] feat: Implementation for should_return_nothing_with_error_message_no_available_position_ticket_when_park_given_a_parking_without_position_and_car

--- src/main/java/com/parkinglot/ParkingLot.java
+++ src/main/java/com/parkinglot/ParkingLot.java
@@ -8,15 +8,19 @@ public class ParkingLot {
 
     private Car car;
     private Map<ParkingTicket,Car> parkedPositions = new HashMap<>();
+    private final int parking_maximum_capacity = 10;
 
     public ParkingTicket park(Car car){
-        if(parkedPositions.size() == 10){
-            return null;
+
+        if(isParkingFull()){
+            throw new NoAvailablePositionException();
         }
         ParkingTicket parkingTicket =  new ParkingTicket();
         this.car = car;
         parkedPositions.put(parkingTicket,car);
         return parkingTicket;
+
+
     }
     public Car fetch(ParkingTicket parkingTicket){
         if(isTicketRecognized(parkingTicket)){
@@ -24,11 +28,16 @@ public class ParkingLot {
         }
 
          final Car car = parkedPositions.get(parkingTicket);
+
          parkedPositions.remove(parkingTicket);
          return car;
     }
     public Boolean isTicketRecognized(ParkingTicket parkingTicket){
+
         return !parkedPositions.containsKey(parkingTicket);
 
+    }
+    public Boolean isParkingFull(){
+    return parkedPositions.size() >= parking_maximum_capacity;
     }
 }
